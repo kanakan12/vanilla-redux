c8dc3bd29bf9fc5c2f1a3d7df33b628da3f8ab9e
kanakan12 <69573636+kanakan12@users.noreply.github.com>
2021-05-10 22:26:51 +0900

    Using react-redux

---
+++ src/routes/Detail.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { connect } from "react-redux";
+
+function Detail({toDos}) {
+    return (
+        <>
+            <h1>{toDos?.text}</h1>
+            <h5>Created at: {toDos?.id}</h5>
+        </>
+    );
+}
+
+function mapStateToProps(state, ownProps){
+    const {match: {params: {id}}} = ownProps;
+    return {toDos: state.find(toDo => toDo.id === parseInt(id))};
+}
+
+export default connect(mapStateToProps)(Detail);
